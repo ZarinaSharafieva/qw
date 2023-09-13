981219340465686d6ffb3c91b336f0587a96f15c
SharafievaZarina <sharafievazarina@gmail.com>
2023-09-13 14:35:48 +0300

    4.2

--- container/js.js
+++ container/js.js
@@ -1,15 +1,18 @@
-let s = prompt('введите стоимость товара')
-let o = prompt('введите количество денег')
+let s1 = prompt('введите стоимость товара')
+let o1 = prompt('введите количество денег')
+
+var s = Number(s1)
+var o = Number(o1)
 const resultEl = document.querySelector('.otvet')
 resultEl.style.color='green'
 if(s == o){
     resultEl.textContent ='Покупка совершена'
 }else{
-    if(s > o){
-        let a = Number(s)-Number(o)
+     if(s > o){
+        let a = s-o
         resultEl.textContent ='Для покупки не хватает '+ a +'р.'
     }else{
-        let b = Number(o)-Number(s)
+        let b = o-s
         resultEl.textContent ='Покупка совершена. Сдача '+ b +'р.'
     }
 }
